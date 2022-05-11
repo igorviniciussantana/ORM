a302f11efd2ff94d742d654a866bf017c9e0f105
igor vinicius <86114583+igorviniciussantana@users.noreply.github.com>
2022-05-11 21:17:50 +0000

    Criação do Model empresas

--- index.js
+++ index.js
@@ -1,35 +1,100 @@
 var express = require("express");
 var app = express();
-var {usuario} = require("./models")
+var { usuario } = require("./models");
+var { empresa } = require("./models")
 
 app.use(express.json());
 app.use(express.urlencoded({
 
   extended: true
-}))
+}));
+//Página Inicial
+app.get("/", function(req, res) {
 
-
-app.get("/", async function(req, res){
-
-var retorna = await usuario.findAll();
- res.json(retorna)
-
-  
-});
-
-app.listen(3000, function(){
-
-console.log("O servidor está supimpa!!!!")
-
-  
-});
-
-app.post("/", function(req, res){
-
-var resultado = usuario.create(req.body);
+  var resultado = "Página Inicial, Bem Vindo!";
   res.json(resultado);
 
 
+});
+//Usuarios
+app.get("/usuarios", async function(req, res) {
 
-  
+  var retorna = await usuario.findAll();
+  res.json(retorna)
+
+
+});
+
+
+app.post("/usuarios", function(req, res) {
+
+  var resultado = usuario.create(req.body);
+  res.json(resultado);
+
+
+});
+
+app.put("/usuarios/:id", async function(req, res) {
+  var atualizar = await usuario.findByPk(req.params.id);
+
+  var resultado = await atualizar.update(req.body);
+  res.json(resultado);
+});
+
+app.get("/usuarios/:id", async function(req, res) {
+  var resultado = await usuario.findByPk(req.params.id);
+
+  res.json(resultado);
+});
+
+app.delete("/usuarios/:id", async function(req, res) {
+  var deletar = await usuario.findByPk(req.params.id);
+   deletar.destroy();
+});
+
+
+//Empresas
+
+app.get("/empresas", async function(req, res) {
+
+  var retorna = await empresa.findAll();
+  res.json(retorna)
+
+
+});
+
+
+app.post("/empresas", function(req, res) {
+
+  var resultado = empresa.create(req.body);
+  res.json(resultado);
+
+
+});
+
+app.put("/empresas/:id", async function(req, res) {
+  var atualizar = await empresa.findByPk(req.params.id);
+
+  var resultado = await atualizar.update(req.body);
+  res.json(resultado);
+});
+
+app.get("/empresas/:id", async function(req, res) {
+  var resultado = await empresa.findByPk(req.params.id);
+
+  res.json(resultado);
+});
+
+app.delete("/empresas/:id", async function(req, res) {
+  var deletar = await empresa.findByPk(req.params.id);
+  deletar.destroy();
+});
+
+
+
+
+app.listen(3000, function() {
+
+  console.log("O servidor está supimpa!!!!")
+
 });
---
+++ migrations/20220511200004-adicionar-empresaid-para-usuario.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn("usuarios", "empresaId", {
+
+allowNull: false,
+type: Sequelize.INTEGER,
+      references: {
+
+        model: "empresas",
+        key: "id"
+        
+      }
+
+
+      
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.addColumn("usuarios", "empresaId")
+  }
+};
